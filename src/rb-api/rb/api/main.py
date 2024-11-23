ad0531da42e5586d700aa90ed9a615a134a9b30c
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2024-11-23 13:31:48 -0500

    fixing bugs

--- src/rb-api/rb/api/main.py
+++ src/rb-api/rb/api/main.py
@@ -1,38 +1,43 @@
 import multiprocessing
-import os, sys
+import os
+import sys
+
 if sys.stdout is None:
-    f = open(r"C:\work\out.log","w")
-    #sys.stdout = open(os.devnull, "w")
+    f = open(r"C:\work\out.log", "w")
+    # sys.stdout = open(os.devnull, "w")
     sys.stdout = f
 if sys.stderr is None:
-    f = open(r"C:\work\err.log","w")
+    f = open(r"C:\work\err.log", "w")
     sys.stdout = f
-    #sys.stderr = open(os.devnull, "w")
+    # sys.stderr = open(os.devnull, "w")
 
+import logging
+
+import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from rb.api import routes
-import uvicorn
-import logging
 
 app = FastAPI(
     title="RescueBoxAPI",
     summary="RescueBox is a set of tools for file system investigations.",
     version="0.1.0",
-    debug = True,
+    debug=True,
     contact={
         "name": "Jagath Jai Kumar",
     },
 )
 
-logger = logging.getLogger('uvicorn.error')
+logger = logging.getLogger("uvicorn.error")
 logger.propagate = True
 logger.setLevel(logging.DEBUG)
 
-@app.get('/log')
+
+@app.get("/log")
 async def main():
-    logger.debug('this is a debug message')
-    return 'log ok'
+    logger.debug("this is a debug message")
+    return "log ok"
+
 
 app.mount(
     "/static",
@@ -50,4 +55,10 @@ if __name__ == "__main__":
 
     multiprocessing.freeze_support()  # For Windows support
     # use_colors=False
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="debug")
+    uvicorn.run(
+        "src.rb-api.rb.api.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        log_level="debug",
+    )
--- src/rb-deepfake-app/rb_deepfake_app/main.py
+++ src/rb-deepfake-app/rb_deepfake_app/main.py
@@ -1,41 +1,54 @@
 import os
+from typing import Annotated, TypedDict
+
 import typer
-from typing import Annotated
-from typing import TypedDict
 
 app = typer.Typer()
+
 
 # Define input types
 class DeepfakeDetectionInputs(TypedDict):
     video_paths: str  # Accepts multiple video file paths
     output_directory: str  # Accepts a directory path
 
+
 class DeepfakeDetectionParameters(TypedDict):
     cuda: str
 
+
 class VideoEvaluator:
-    def __init__(self, model_path=None, output_path='.', cuda=True):
+    def __init__(self, model_path=None, output_path=".", cuda=True):
         self.output_path = output_path
         self.cuda = cuda
 
     def evaluate_video(self, video_path, start_frame=0, end_frame=None):
-        print(f'Starting: {video_path}')
+        print(f"Starting: {video_path}")
         video_fn = f"{os.path.splitext(os.path.basename(video_path))[0]}_processed.avi"
         processed_video_path = os.path.join(self.output_path, video_fn)
         os.makedirs(self.output_path, exist_ok=True)
         return processed_video_path
 
-@app.command
+
+@app.command()
 def detect_deepfake(
-    # inputs: DeepfakeDetectionInputs, parameters: DeepfakeDetectionParameters    
+    # inputs: DeepfakeDetectionInputs, parameters: DeepfakeDetectionParameters
     # Initialize the VideoEvaluator with model and output paths
-    video_paths: str = typer.Argument(..., help="The path to input video files eg: path/to/your/video.folder"),
-    model_path: str = typer.Argument(..., help="The path to the pre-trained model eg: path/to/your/model.pth"),
-    output_path: str = typer.Argument(..., help="The path Directory to save processed videos"),
-    cuda_flag: str = Annotated[bool, typer.Option("-cuda",help="cuda flag boolean")] == "True"
-                               ) -> str:
-    evaluator = VideoEvaluator(model_path=model_path, output_path=output_path, cuda=cuda_flag)
-    
+    video_paths: str = typer.Argument(
+        ..., help="The path to input video files eg: path/to/your/video.folder"
+    ),
+    model_path: str = typer.Argument(
+        ..., help="The path to the pre-trained model eg: path/to/your/model.pth"
+    ),
+    output_path: str = typer.Argument(
+        ..., help="The path Directory to save processed videos"
+    ),
+    cuda_flag: str = Annotated[bool, typer.Option("-cuda", help="cuda flag boolean")]
+    == "True",
+) -> str:
+    evaluator = VideoEvaluator(
+        model_path=model_path, output_path=output_path, cuda=cuda_flag
+    )
+
     # Process each video file path and store the output paths
     output_paths = []
     for video_path in video_paths.files:
@@ -50,6 +63,16 @@ def detect_deepfake(
             print(f"Failed to process video: {video_path.path}")
     # Process each video file path and store the output path
     return output_paths
+
+
+# @app.command()
+# def test(name: str) -> str:
+#     print(f"Hello world {name}")
+#     return True
+
+
+if __name__ == "__main__":
+    app()
 
 ''''
 
